8865b0def66687424bdbf00b3324d2b21712e0c6
Lily Akhmetova <lilyakhmetovamail@gmail.com>
2020-04-25 13:17:32 +0300

    мобилка

---
+++ js/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  createCanvas(1000, 800, WEBGL);
+
+  frameRate(10);
+}
+
+function draw() {
+  background(0);
+
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
+  rotateZ(frameCount * 0.05);
+
+  for (let i = 0; i < 40; i++) {
+  createBubble(0, 0, 12 * i + 10, 12 * i + 10, random(0.1, 2), {r: random(mouseX / 3, 255), g: 0, b: random(mouseX / 3, 255), });
+  }
+}
+
+
+function createBubble(x, y, d, e, s, col) {
+ strokeWeight(s);
+  noFill();
+  stroke(col.r, col.g, col.b);
+  rectMode(CENTER);
+  rect(x + random(-0.5, +0.5), y + random(-0.5, +0.5), d, e);
+
+}
+
+function mousePressed() {
+  background(0);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
